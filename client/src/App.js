fa2c42b0c9f224be60aac8e707d578cdd13ce023
Tarique Smith <tpsmith1992@gmail.com>
2021-10-14 12:25:25 -0400

    Added material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <Button>I'm a Button </Button>
+      <h1>Page Count:</h1>
     </div>
   );
 }
