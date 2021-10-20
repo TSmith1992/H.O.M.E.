ab2389e9a7fcb258ee4a1fc9e6f03da77147ce28
Tarique Smith <tpsmith1992@gmail.com>
2021-10-20 16:02:25 -0400

    Final functionality touches

--- client/src/components/LeaderMove.js
+++ client/src/components/LeaderMove.js
@@ -4,7 +4,7 @@ import LeaderMoveShelter from "./LeaderMoveShelter";
 export default function LeaderMove({ currentUser, shelters }) {
   return (
     <div>
-        <p>As a Shelter Lead, you may move migrants to different shelters you run in order to ease capacity for certain shelters.</p>
+        <p>As a Shelter Lead, you may move migrants to different shelters you manage in order to ease capacity for certain shelters.</p>
       {currentUser.shelters.map((shelter) => (
         <LeaderMoveShelter key={shelter.name} shelter={shelter} currentUser={currentUser}/>
       ))}
